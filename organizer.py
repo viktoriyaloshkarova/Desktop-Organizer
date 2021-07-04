b0ec9601fea0052c6e71c21eeebb2dd968da8478
viktoriyaloshkarova <viktoriyaloshkarova0419@gmail.com>
2021-07-03 21:17:18 -0400

    Created directories for each file format

--- organizer.py
+++ organizer.py
@@ -1,1 +1,15 @@
-hello
+import os
+from pathlib import Path
+
+DIRECTORIES = {
+    "Images": [".jpeg", ".jpg", ".gif", ".bmp", ".png", ".psd", ".heic"],
+    "Videos": [".mov", ".mp4", ".webm", ".mng", ".mpg", ".mpeg"],
+    "Documents": [".doc", ".docx", ".fdf", ".dox"],
+    "PDF": [".pdf"]
+}
+
+FILE_FORMATS = {
+    file_format: directory
+    for directory, file_formats in DIRECTORIES.items()
+    for file_name in file_formats
+}
