d4e4a18a47374dcaab1c1304992982f561d64e1b
viktoriyaloshkarova <viktoriyaloshkarova0419@gmail.com>
2021-07-03 21:29:46 -0400

    implemented a function to move file to a directory fitting for it

--- organizer.py
+++ organizer.py
@@ -13,3 +13,23 @@ FILE_FORMATS = {
     for directory, file_formats in DIRECTORIES.items()
     for file_name in file_formats
 }
+
+def organizer():
+    for entry in os.scandir():
+        if entry.is_dir():
+            continue
+        file_path = Path(entry)
+        file_format = file_path.suffix.lower()
+        if file_format in FILE_FORMATS:
+            directory_path = Path(FILE_FORMATS[file_format])
+            directory_path.mkdir(exist_ok=True)
+            file_path.rename(directory_path.joinpath(file_path))
+
+        for dir in os.scandir():
+            try:
+                os.rmdir(dir)
+            except:
+                pass
+
+if __name__ == "__main__":
+    organizer()
